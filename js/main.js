dbb1a6be563318937b2a34dbcfea3744e0eb2488
Safarmurod999 <supermen0904@gmail.com>
2023-02-17 11:29:12 +0500

    navbar end

--- js/main.js
+++ js/main.js
@@ -1,13 +1,89 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
   }
-  
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";  
+}
+
+includeHTML();
+
+
+
+
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.style.opacity = 1;
+  } else {
+    backtop.style.opacity = 0;
   }
+}
+
+
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+
+function openNavbar() {
+  document.getElementById("navbar-responsive").style.left = "0";
+  document.body.style.backgroundColor="rgba(0,0,0,0.4)";  
+  document.getElementById("offCanvas").style.display="block";
+  // document.getElementById("navbar-responsive").style.top = "0";
+}
+
+function closeNavbar() {
+  document.getElementById("navbar-responsive").style.left = "-1500px";
+  // document.getElementById("navbar-responsive").style.top = "-100%";
+  document.body.style.backgroundColor = "white";
+}
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    navbar.classList.add("navbar-shrink");
+    // navbar.style.padding = "60px 0 15px";
+  } else {
+    navbar.classList.remove("navbar-shrink");
+    // navbar.style.padding = "73px 0 20px";
+  }
+}
+
